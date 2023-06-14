79bbfe82907aca3755439431408d067832efabf7
jochou02 <jochou02@gmail.com>
2023-06-14 11:23:32 -0700

    Model is functional, ready for deployment

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    fetch('https://sports-modeling.onrender.com/health')
-        .then(response => response.json())
-        .then(data => {
-            const contentDiv = document.getElementById('content');
-            contentDiv.textContent = JSON.stringify(data);
-        });
+document.getElementById('submit').addEventListener('click', function() {
+    var player = document.getElementById('player').value;
+    var wins = parseInt(document.getElementById('wins').value);
+    var losses = parseInt(document.getElementById('losses').value);
+    
+    fetch('https://sports-modeling.onrender.com/proj_kills', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            player: player,
+            wins: wins,
+            losses: losses,
+        }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('results').textContent = JSON.stringify(data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 });
+
+
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     fetch('https://sports-modeling.onrender.com/health')
+//         .then(response => response.json())
+//         .then(data => {
+//             const contentDiv = document.getElementById('content');
+//             contentDiv.textContent = JSON.stringify(data);
+//         });
+// });
