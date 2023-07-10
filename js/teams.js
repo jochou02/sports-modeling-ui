cbd89910eeb64979fc481fd11738729e82cfcd21
jochou02 <jochou02@gmail.com>
2023-07-09 21:48:48 -0700

    Added autofill and team projections

--- js/teams.js
+++ js/teams.js
@@ -1,55 +1,17 @@
 /*
 ------------------------------------------------------------------------------------
-BACKGROUND IMAGE ROTATING FUNCTIONALITY 
-------------------------------------------------------------------------------------
-*/
-
-document.addEventListener("DOMContentLoaded", function(){
-
-    var images = [
-        'images/DRX.jpg', 
-        'images/AsolBackground.jpg', 
-        'images/Garen.jpg',
-        'images/Hecarim.jpg',
-        'images/Irelia.jpg',
-        'images/Rakan.jpg',
-        'images/Aatrox.jpg',
-    ];
-    var index = 0;
-
-    function changeBackground() {
-        document.getElementById('background-wrapper').style.backgroundImage = 'url(' + images[index] + ')';
-    
-        // Change the index to the next image, loop back to the start if necessary
-        index = (index + 1) % images.length;
-    }
-
-    // Call the function once immediately, then again every 5 seconds
-    changeBackground();
-    setInterval(changeBackground, 5000);
-
-    images.forEach(function(src) {
-        var img = new Image();
-        img.src = src;
-    });
-});
-
-/*
-------------------------------------------------------------------------------------
 SUBMIT AND LOADING ANIMATION FUNCTION
 ------------------------------------------------------------------------------------
 */
-
-
 document.getElementById('submit').addEventListener('click', function() {
 
-    var player = document.getElementById('player').value;
+    var team = document.getElementById('team').value;
     var wins = Number(document.getElementById('wins').value);
     var losses = Number(document.getElementById('losses').value);
 
     // Player name input validation check
-    if (player.length == 0) {
-        alert('Please enter a player!');
+    if (team.length == 0) {
+        alert('Please enter a team!');
         return;
     }
 
@@ -64,13 +26,13 @@ document.getElementById('submit').addEventListener('click', function() {
     hidePlayerNotFoundErr();
     showLoading();
     
-    fetch('https://sports-modeling.onrender.com/player_info', {
+    fetch('https://sports-modeling.onrender.com/team_info', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            player: player,
+            team: team,
             wins: wins,
             losses: losses,
         }),
@@ -82,9 +44,19 @@ document.getElementById('submit').addEventListener('click', function() {
         return response.json();
     })
     .then(data => {
-        document.getElementById('result-name').textContent = `${player} [ ${wins} - ${losses} ]`;
-        document.getElementById('result-projection').innerHTML = `Projected Kills: ${data.proj_kills}<br>Projected Deaths: ${data.proj_deaths}`;
-        
+        var projection = data.projection;
+        var resultString = '';
+    
+        for (var player in projection) {
+            if (projection.hasOwnProperty(player)) {
+                var stats = projection[player].join(' / ');
+                resultString += `${player} KDA: ${stats}<br>`;
+            }
+        }
+    
+        document.getElementById('result-name').textContent = `${team} [ ${wins} - ${losses} ]`;
+        document.getElementById('result-projection').innerHTML = resultString;
+    
         hideLoading();
         showSubmit();
         showOutput();
@@ -125,13 +97,11 @@ function hideOutput(){
 // Show the loading indicator
 function showLoading() {
     document.getElementsByClassName('lds-ellipsis')[0].style.display = 'block';
-    document.getElementById('load-msg').style.display = 'block';
 }
 
 // Hide loading indicator
 function hideLoading() {
     document.getElementsByClassName('lds-ellipsis')[0].style.display = 'none';
-    document.getElementById('load-msg').style.display = 'none';
 }
 
 // Show submit button
@@ -152,7 +122,7 @@ PLAYER AUTOFILL FUNCTIONALITY
 
 let playersNames = [];
 
-fetch('./all_players.csv')
+fetch('./all_teams.csv')
     .then(response => response.text())
     .then(data => {
         let rows = data.split('\n'); // Split CSV into rows
@@ -164,11 +134,11 @@ fetch('./all_players.csv')
         });
     });
 
-document.getElementById('player').addEventListener('input', function() {
+document.getElementById('team').addEventListener('input', function() {
     let input = this.value;
 
     // Clear old options
-    let datalist = document.getElementById('players');
+    let datalist = document.getElementById('teams');
     datalist.innerHTML = '';
 
     // Filter players
